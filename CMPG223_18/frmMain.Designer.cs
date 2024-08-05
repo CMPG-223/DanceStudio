afa2235287a468b72f92f5e0d0e0b92f3b664087
LeaniBoshoff <boshoffleani@gmail.com>
2024-08-05 16:11:18 +0200

    Project

---
+++ CMPG223_18/frmMain.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace CMPG223_18
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pcbLogo = new System.Windows.Forms.PictureBox();
+            this.btnDancers = new System.Windows.Forms.Button();
+            this.btnEnrol = new System.Windows.Forms.Button();
+            this.btnClasses = new System.Windows.Forms.Button();
+            this.btnPayments = new System.Windows.Forms.Button();
+            this.btnTeachers = new System.Windows.Forms.Button();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.btnLogOut = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbLogo
+            // 
+            this.pcbLogo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pcbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pcbLogo.Image")));
+            this.pcbLogo.InitialImage = null;
+            this.pcbLogo.Location = new System.Drawing.Point(177, 36);
+            this.pcbLogo.Name = "pcbLogo";
+            this.pcbLogo.Size = new System.Drawing.Size(573, 302);
+            this.pcbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbLogo.TabIndex = 1;
+            this.pcbLogo.TabStop = false;
+            // 
+            // btnDancers
+            // 
+            this.btnDancers.BackColor = System.Drawing.Color.OldLace;
+            this.btnDancers.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDancers.Location = new System.Drawing.Point(147, 389);
+            this.btnDancers.Name = "btnDancers";
+            this.btnDancers.Size = new System.Drawing.Size(206, 49);
+            this.btnDancers.TabIndex = 2;
+            this.btnDancers.Text = "Dancers";
+            this.btnDancers.UseVisualStyleBackColor = false;
+            // 
+            // btnEnrol
+            // 
+            this.btnEnrol.BackColor = System.Drawing.Color.OldLace;
+            this.btnEnrol.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnrol.Location = new System.Drawing.Point(147, 460);
+            this.btnEnrol.Name = "btnEnrol";
+            this.btnEnrol.Size = new System.Drawing.Size(206, 49);
+            this.btnEnrol.TabIndex = 3;
+            this.btnEnrol.Text = "Enrol";
+            this.btnEnrol.UseVisualStyleBackColor = false;
+            // 
+            // btnClasses
+            // 
+            this.btnClasses.BackColor = System.Drawing.Color.OldLace;
+            this.btnClasses.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClasses.Location = new System.Drawing.Point(357, 389);
+            this.btnClasses.Name = "btnClasses";
+            this.btnClasses.Size = new System.Drawing.Size(206, 49);
+            this.btnClasses.TabIndex = 4;
+            this.btnClasses.Text = "Classes";
+            this.btnClasses.UseVisualStyleBackColor = false;
+            // 
+            // btnPayments
+            // 
+            this.btnPayments.BackColor = System.Drawing.Color.OldLace;
+            this.btnPayments.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPayments.Location = new System.Drawing.Point(357, 460);
+            this.btnPayments.Name = "btnPayments";
+            this.btnPayments.Size = new System.Drawing.Size(206, 49);
+            this.btnPayments.TabIndex = 5;
+            this.btnPayments.Text = "Payments";
+            this.btnPayments.UseVisualStyleBackColor = false;
+            // 
+            // btnTeachers
+            // 
+            this.btnTeachers.BackColor = System.Drawing.Color.OldLace;
+            this.btnTeachers.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTeachers.Location = new System.Drawing.Point(568, 389);
+            this.btnTeachers.Name = "btnTeachers";
+            this.btnTeachers.Size = new System.Drawing.Size(206, 49);
+            this.btnTeachers.TabIndex = 6;
+            this.btnTeachers.Text = "Teachers";
+            this.btnTeachers.UseVisualStyleBackColor = false;
+            // 
+            // btnReports
+            // 
+            this.btnReports.BackColor = System.Drawing.Color.OldLace;
+            this.btnReports.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReports.Location = new System.Drawing.Point(568, 460);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(206, 49);
+            this.btnReports.TabIndex = 7;
+            this.btnReports.Text = "Reports";
+            this.btnReports.UseVisualStyleBackColor = false;
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.BackColor = System.Drawing.Color.OldLace;
+            this.btnLogOut.Font = new System.Drawing.Font("Modern No. 20", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogOut.Location = new System.Drawing.Point(860, 540);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(84, 31);
+            this.btnLogOut.TabIndex = 8;
+            this.btnLogOut.Text = "Log Out";
+            this.btnLogOut.UseVisualStyleBackColor = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(956, 583);
+            this.Controls.Add(this.btnLogOut);
+            this.Controls.Add(this.btnReports);
+            this.Controls.Add(this.btnTeachers);
+            this.Controls.Add(this.btnPayments);
+            this.Controls.Add(this.btnClasses);
+            this.Controls.Add(this.btnEnrol);
+            this.Controls.Add(this.btnDancers);
+            this.Controls.Add(this.pcbLogo);
+            this.Name = "frmMain";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbLogo;
+        private System.Windows.Forms.Button btnDancers;
+        private System.Windows.Forms.Button btnEnrol;
+        private System.Windows.Forms.Button btnClasses;
+        private System.Windows.Forms.Button btnPayments;
+        private System.Windows.Forms.Button btnTeachers;
+        private System.Windows.Forms.Button btnReports;
+        private System.Windows.Forms.Button btnLogOut;
+    }
+}
