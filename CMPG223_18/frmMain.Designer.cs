7fb63868ff38c81241a3857926414b2e774cb5bc
AnrivT <email adress>
2024-08-28 18:33:32 +0200

    Latest update

--- CMPG223_18/frmMain.Designer.cs
+++ CMPG223_18/frmMain.Designer.cs
@@ -48,9 +48,10 @@ namespace CMPG223_18
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pcbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pcbLogo.Image")));
             this.pcbLogo.InitialImage = null;
-            this.pcbLogo.Location = new System.Drawing.Point(177, 36);
+            this.pcbLogo.Location = new System.Drawing.Point(118, 23);
+            this.pcbLogo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pcbLogo.Name = "pcbLogo";
-            this.pcbLogo.Size = new System.Drawing.Size(573, 302);
+            this.pcbLogo.Size = new System.Drawing.Size(382, 196);
             this.pcbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pcbLogo.TabIndex = 1;
             this.pcbLogo.TabStop = false;
@@ -59,85 +60,98 @@ namespace CMPG223_18
             // 
             this.btnDancers.BackColor = System.Drawing.Color.OldLace;
             this.btnDancers.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDancers.Location = new System.Drawing.Point(147, 389);
+            this.btnDancers.Location = new System.Drawing.Point(98, 253);
+            this.btnDancers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDancers.Name = "btnDancers";
-            this.btnDancers.Size = new System.Drawing.Size(206, 49);
+            this.btnDancers.Size = new System.Drawing.Size(137, 32);
             this.btnDancers.TabIndex = 2;
             this.btnDancers.Text = "Dancers";
             this.btnDancers.UseVisualStyleBackColor = false;
+            this.btnDancers.Click += new System.EventHandler(this.btnDancers_Click);
             // 
             // btnEnrol
             // 
             this.btnEnrol.BackColor = System.Drawing.Color.OldLace;
             this.btnEnrol.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEnrol.Location = new System.Drawing.Point(147, 460);
+            this.btnEnrol.Location = new System.Drawing.Point(98, 299);
+            this.btnEnrol.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEnrol.Name = "btnEnrol";
-            this.btnEnrol.Size = new System.Drawing.Size(206, 49);
+            this.btnEnrol.Size = new System.Drawing.Size(137, 32);
             this.btnEnrol.TabIndex = 3;
             this.btnEnrol.Text = "Enrol";
             this.btnEnrol.UseVisualStyleBackColor = false;
+            this.btnEnrol.Click += new System.EventHandler(this.btnEnrol_Click);
             // 
             // btnClasses
             // 
             this.btnClasses.BackColor = System.Drawing.Color.OldLace;
             this.btnClasses.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClasses.Location = new System.Drawing.Point(357, 389);
+            this.btnClasses.Location = new System.Drawing.Point(238, 253);
+            this.btnClasses.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClasses.Name = "btnClasses";
-            this.btnClasses.Size = new System.Drawing.Size(206, 49);
+            this.btnClasses.Size = new System.Drawing.Size(137, 32);
             this.btnClasses.TabIndex = 4;
             this.btnClasses.Text = "Classes";
             this.btnClasses.UseVisualStyleBackColor = false;
+            this.btnClasses.Click += new System.EventHandler(this.btnClasses_Click);
             // 
             // btnPayments
             // 
             this.btnPayments.BackColor = System.Drawing.Color.OldLace;
             this.btnPayments.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPayments.Location = new System.Drawing.Point(357, 460);
+            this.btnPayments.Location = new System.Drawing.Point(238, 299);
+            this.btnPayments.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnPayments.Name = "btnPayments";
-            this.btnPayments.Size = new System.Drawing.Size(206, 49);
+            this.btnPayments.Size = new System.Drawing.Size(137, 32);
             this.btnPayments.TabIndex = 5;
             this.btnPayments.Text = "Payments";
             this.btnPayments.UseVisualStyleBackColor = false;
+            this.btnPayments.Click += new System.EventHandler(this.btnPayments_Click);
             // 
             // btnTeachers
             // 
             this.btnTeachers.BackColor = System.Drawing.Color.OldLace;
             this.btnTeachers.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTeachers.Location = new System.Drawing.Point(568, 389);
+            this.btnTeachers.Location = new System.Drawing.Point(379, 253);
+            this.btnTeachers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnTeachers.Name = "btnTeachers";
-            this.btnTeachers.Size = new System.Drawing.Size(206, 49);
+            this.btnTeachers.Size = new System.Drawing.Size(137, 32);
             this.btnTeachers.TabIndex = 6;
             this.btnTeachers.Text = "Teachers";
             this.btnTeachers.UseVisualStyleBackColor = false;
+            this.btnTeachers.Click += new System.EventHandler(this.btnTeachers_Click);
             // 
             // btnReports
             // 
             this.btnReports.BackColor = System.Drawing.Color.OldLace;
             this.btnReports.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReports.Location = new System.Drawing.Point(568, 460);
+            this.btnReports.Location = new System.Drawing.Point(379, 299);
+            this.btnReports.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnReports.Name = "btnReports";
-            this.btnReports.Size = new System.Drawing.Size(206, 49);
+            this.btnReports.Size = new System.Drawing.Size(137, 32);
             this.btnReports.TabIndex = 7;
             this.btnReports.Text = "Reports";
             this.btnReports.UseVisualStyleBackColor = false;
+            this.btnReports.Click += new System.EventHandler(this.btnReports_Click);
             // 
             // btnLogOut
             // 
             this.btnLogOut.BackColor = System.Drawing.Color.OldLace;
             this.btnLogOut.Font = new System.Drawing.Font("Modern No. 20", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogOut.Location = new System.Drawing.Point(860, 540);
+            this.btnLogOut.Location = new System.Drawing.Point(573, 351);
+            this.btnLogOut.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogOut.Name = "btnLogOut";
-            this.btnLogOut.Size = new System.Drawing.Size(84, 31);
+            this.btnLogOut.Size = new System.Drawing.Size(56, 20);
             this.btnLogOut.TabIndex = 8;
             this.btnLogOut.Text = "Log Out";
             this.btnLogOut.UseVisualStyleBackColor = false;
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(956, 583);
+            this.ClientSize = new System.Drawing.Size(637, 379);
             this.Controls.Add(this.btnLogOut);
             this.Controls.Add(this.btnReports);
             this.Controls.Add(this.btnTeachers);
@@ -146,6 +160,7 @@ namespace CMPG223_18
             this.Controls.Add(this.btnEnrol);
             this.Controls.Add(this.btnDancers);
             this.Controls.Add(this.pcbLogo);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmMain";
             this.Text = "Main";
             ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).EndInit();
--- CMPG223_18/frmReports.Designer.cs
+++ CMPG223_18/frmReports.Designer.cs
@@ -29,6 +29,7 @@ namespace CMPG223_18
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReports));
             this.btnBack = new System.Windows.Forms.Button();
             this.btnLogOut = new System.Windows.Forms.Button();
@@ -42,6 +43,11 @@ namespace CMPG223_18
             this.cmbSortBy = new System.Windows.Forms.ComboBox();
             this.lblSort = new System.Windows.Forms.Label();
             this.dgvReports = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblExplain = new System.Windows.Forms.Label();
+            this.ttChoose = new System.Windows.Forms.ToolTip(this.components);
             this.gpbReports.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvReports)).BeginInit();
             this.SuspendLayout();
@@ -51,86 +57,103 @@ namespace CMPG223_18
             this.btnBack.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.btnBack.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBack.BackgroundImage")));
             this.btnBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnBack.Location = new System.Drawing.Point(12, 12);
+            this.btnBack.Location = new System.Drawing.Point(8, 8);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(2);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(36, 36);
+            this.btnBack.Size = new System.Drawing.Size(24, 23);
             this.btnBack.TabIndex = 7;
             this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // btnLogOut
             // 
             this.btnLogOut.BackColor = System.Drawing.Color.OldLace;
             this.btnLogOut.Font = new System.Drawing.Font("Modern No. 20", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogOut.Location = new System.Drawing.Point(827, 567);
+            this.btnLogOut.Location = new System.Drawing.Point(717, 375);
+            this.btnLogOut.Margin = new System.Windows.Forms.Padding(2);
             this.btnLogOut.Name = "btnLogOut";
-            this.btnLogOut.Size = new System.Drawing.Size(84, 31);
+            this.btnLogOut.Size = new System.Drawing.Size(56, 20);
             this.btnLogOut.TabIndex = 12;
             this.btnLogOut.Text = "Log Out";
             this.btnLogOut.UseVisualStyleBackColor = false;
             // 
             // dtpEndDate
             // 
-            this.dtpEndDate.Location = new System.Drawing.Point(65, 444);
+            this.dtpEndDate.Location = new System.Drawing.Point(82, 306);
+            this.dtpEndDate.Margin = new System.Windows.Forms.Padding(2);
             this.dtpEndDate.Name = "dtpEndDate";
-            this.dtpEndDate.Size = new System.Drawing.Size(200, 26);
+            this.dtpEndDate.Size = new System.Drawing.Size(135, 20);
             this.dtpEndDate.TabIndex = 20;
+            this.dtpEndDate.ValueChanged += new System.EventHandler(this.dtpEndDate_ValueChanged);
             // 
             // lblEndDate
             // 
             this.lblEndDate.AutoSize = true;
-            this.lblEndDate.Location = new System.Drawing.Point(65, 421);
+            this.lblEndDate.Location = new System.Drawing.Point(82, 291);
+            this.lblEndDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblEndDate.Name = "lblEndDate";
-            this.lblEndDate.Size = new System.Drawing.Size(77, 20);
+            this.lblEndDate.Size = new System.Drawing.Size(52, 13);
             this.lblEndDate.TabIndex = 19;
             this.lblEndDate.Text = "End Date";
             // 
             // dtpStartDate
             // 
-            this.dtpStartDate.Location = new System.Drawing.Point(65, 353);
+            this.dtpStartDate.Location = new System.Drawing.Point(82, 246);
+            this.dtpStartDate.Margin = new System.Windows.Forms.Padding(2);
             this.dtpStartDate.Name = "dtpStartDate";
-            this.dtpStartDate.Size = new System.Drawing.Size(200, 26);
+            this.dtpStartDate.Size = new System.Drawing.Size(135, 20);
             this.dtpStartDate.TabIndex = 18;
+            this.dtpStartDate.Value = new System.DateTime(2024, 1, 1, 0, 0, 0, 0);
+            this.dtpStartDate.ValueChanged += new System.EventHandler(this.dtpStartDate_ValueChanged);
             // 
             // gpbReports
             // 
             this.gpbReports.BackColor = System.Drawing.Color.WhiteSmoke;
             this.gpbReports.Controls.Add(this.rdbOutPay);
             this.gpbReports.Controls.Add(this.rdbTop3Dance);
-            this.gpbReports.Location = new System.Drawing.Point(66, 78);
+            this.gpbReports.Location = new System.Drawing.Point(44, 51);
+            this.gpbReports.Margin = new System.Windows.Forms.Padding(2);
             this.gpbReports.Name = "gpbReports";
-            this.gpbReports.Size = new System.Drawing.Size(211, 147);
+            this.gpbReports.Padding = new System.Windows.Forms.Padding(2);
+            this.gpbReports.Size = new System.Drawing.Size(227, 105);
             this.gpbReports.TabIndex = 17;
             this.gpbReports.TabStop = false;
-            this.gpbReports.Text = "Reports";
+            this.gpbReports.Text = "Please choose what report you want to see";
+            this.ttChoose.SetToolTip(this.gpbReports, "Choose what report you want to see here");
             // 
             // rdbOutPay
             // 
             this.rdbOutPay.AutoSize = true;
-            this.rdbOutPay.Location = new System.Drawing.Point(7, 90);
+            this.rdbOutPay.Location = new System.Drawing.Point(5, 58);
+            this.rdbOutPay.Margin = new System.Windows.Forms.Padding(2);
             this.rdbOutPay.Name = "rdbOutPay";
-            this.rdbOutPay.Size = new System.Drawing.Size(194, 24);
+            this.rdbOutPay.Size = new System.Drawing.Size(130, 17);
             this.rdbOutPay.TabIndex = 1;
             this.rdbOutPay.TabStop = true;
             this.rdbOutPay.Text = "Outstanding payments";
             this.rdbOutPay.UseVisualStyleBackColor = true;
+            this.rdbOutPay.CheckedChanged += new System.EventHandler(this.rdbTop3Dance_CheckedChanged);
             // 
             // rdbTop3Dance
             // 
             this.rdbTop3Dance.AutoSize = true;
-            this.rdbTop3Dance.Location = new System.Drawing.Point(7, 41);
+            this.rdbTop3Dance.Location = new System.Drawing.Point(5, 27);
+            this.rdbTop3Dance.Margin = new System.Windows.Forms.Padding(2);
             this.rdbTop3Dance.Name = "rdbTop3Dance";
-            this.rdbTop3Dance.Size = new System.Drawing.Size(133, 24);
+            this.rdbTop3Dance.Size = new System.Drawing.Size(93, 17);
             this.rdbTop3Dance.TabIndex = 0;
             this.rdbTop3Dance.TabStop = true;
             this.rdbTop3Dance.Text = "Top 3 Dances";
             this.rdbTop3Dance.UseVisualStyleBackColor = true;
+            this.rdbTop3Dance.CheckedChanged += new System.EventHandler(this.rdbTop3Dance_CheckedChanged);
             // 
             // lblStartDate
             // 
             this.lblStartDate.AutoSize = true;
-            this.lblStartDate.Location = new System.Drawing.Point(65, 330);
+            this.lblStartDate.Location = new System.Drawing.Point(82, 231);
+            this.lblStartDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblStartDate.Name = "lblStartDate";
-            this.lblStartDate.Size = new System.Drawing.Size(83, 20);
+            this.lblStartDate.Size = new System.Drawing.Size(55, 13);
             this.lblStartDate.TabIndex = 16;
             this.lblStartDate.Text = "Start Date";
             // 
@@ -139,20 +162,23 @@ namespace CMPG223_18
             this.cmbSortBy.FormattingEnabled = true;
             this.cmbSortBy.Items.AddRange(new object[] {
             "Alphabetical (A-Z)",
-            "(Z-A)",
-            "Age (oldest to youngest)",
-            "Age"});
-            this.cmbSortBy.Location = new System.Drawing.Point(65, 266);
+            "Alphabetical (Z-A)",
+            "Most in debt",
+            "Least in debt"});
+            this.cmbSortBy.Location = new System.Drawing.Point(82, 190);
+            this.cmbSortBy.Margin = new System.Windows.Forms.Padding(2);
             this.cmbSortBy.Name = "cmbSortBy";
-            this.cmbSortBy.Size = new System.Drawing.Size(200, 28);
+            this.cmbSortBy.Size = new System.Drawing.Size(135, 21);
             this.cmbSortBy.TabIndex = 15;
+            this.cmbSortBy.SelectedIndexChanged += new System.EventHandler(this.cmbSortBy_SelectedIndexChanged);
             // 
             // lblSort
             // 
             this.lblSort.AutoSize = true;
-            this.lblSort.Location = new System.Drawing.Point(61, 243);
+            this.lblSort.Location = new System.Drawing.Point(80, 175);
+            this.lblSort.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSort.Name = "lblSort";
-            this.lblSort.Size = new System.Drawing.Size(61, 20);
+            this.lblSort.Size = new System.Drawing.Size(41, 13);
             this.lblSort.TabIndex = 14;
             this.lblSort.Text = "Sort By";
             // 
@@ -161,19 +187,75 @@ namespace CMPG223_18
             this.dgvReports.AllowUserToOrderColumns = true;
             this.dgvReports.BackgroundColor = System.Drawing.Color.WhiteSmoke;
             this.dgvReports.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvReports.Location = new System.Drawing.Point(370, 78);
+            this.dgvReports.Location = new System.Drawing.Point(371, 51);
+            this.dgvReports.Margin = new System.Windows.Forms.Padding(2);
             this.dgvReports.Name = "dgvReports";
             this.dgvReports.RowHeadersWidth = 62;
             this.dgvReports.RowTemplate.Height = 28;
-            this.dgvReports.Size = new System.Drawing.Size(422, 482);
+            this.dgvReports.Size = new System.Drawing.Size(281, 313);
             this.dgvReports.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(323, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 31);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Reports";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.BackColor = System.Drawing.Color.OldLace;
+            this.btnExport.Font = new System.Drawing.Font("Modern No. 20", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.Location = new System.Drawing.Point(720, 48);
+            this.btnExport.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(56, 20);
+            this.btnExport.TabIndex = 22;
+            this.btnExport.Text = "Print Report";
+            this.btnExport.UseVisualStyleBackColor = false;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(720, 128);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Explanation:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblExplain
+            // 
+            this.lblExplain.AutoSize = true;
+            this.lblExplain.Location = new System.Drawing.Point(720, 152);
+            this.lblExplain.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblExplain.Name = "lblExplain";
+            this.lblExplain.Size = new System.Drawing.Size(65, 13);
+            this.lblExplain.TabIndex = 24;
+            this.lblExplain.Text = "Explanation:";
+            // 
+            // ttChoose
+            // 
+            this.ttChoose.Tag = "Choose either of the buttons to get that report";
             // 
             // frmReports
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(923, 610);
+            this.ClientSize = new System.Drawing.Size(910, 429);
+            this.Controls.Add(this.lblExplain);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dtpEndDate);
             this.Controls.Add(this.lblEndDate);
             this.Controls.Add(this.dtpStartDate);
@@ -184,8 +266,10 @@ namespace CMPG223_18
             this.Controls.Add(this.dgvReports);
             this.Controls.Add(this.btnLogOut);
             this.Controls.Add(this.btnBack);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmReports";
             this.Text = "Reports";
+            this.Load += new System.EventHandler(this.frmReports_Load);
             this.gpbReports.ResumeLayout(false);
             this.gpbReports.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvReports)).EndInit();
@@ -208,5 +292,10 @@ namespace CMPG223_18
         private System.Windows.Forms.ComboBox cmbSortBy;
         private System.Windows.Forms.Label lblSort;
         private System.Windows.Forms.DataGridView dgvReports;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblExplain;
+        private System.Windows.Forms.ToolTip ttChoose;
     }
 }
